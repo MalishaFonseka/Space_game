6701434ed2143a02df29557ce0213b9e8eb593cc
Malisha Fonseka <janithmalishafonseka@gmail.com>
2019-01-01 20:22:33 +0530

    Added comments

--- gamefire/medipack.cpp
+++ gamefire/medipack.cpp
@@ -7,14 +7,15 @@
 
 medipack::medipack(QGraphicsItem *parent): QObject(), QGraphicsPixmapItem(parent)
 {
+    //creating medipack staring points base on a randomm number assigned to a variable
     int rand_num = rand() % 500;
     setPos(rand_num,0);
 
+    //setting the edipack image
     setPixmap(QPixmap(":/images/medipack_1.jpg"));
 
-
+    //connecting the medipack with movefunction
     QTimer * timer = new QTimer(this);
-
     connect(timer,SIGNAL(timeout()),this,SLOT(move()));
     timer->start(60);
 }
@@ -23,6 +24,7 @@ void medipack::move()
 {
 
     setPos(x(),y()+5);
+    //medipack is removed when passes the boudary
     if(pos().y() >600){
       scene()->removeItem(this);
       delete this;
